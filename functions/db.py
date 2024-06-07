d85856cecd49360c13f8c67c3343701fef820bae
Simon Baars <simon.mailadres@gmail.com>
2024-06-06 20:56:23 -0700

    database functions

---
+++ functions/db.py
@@ -0,0 +1,25 @@
+import firebase_admin
+from firebase_admin import firestore
+
+def get_firestore():
+    if not firebase_admin._apps:
+        firebase_admin.initialize_app()
+    return firestore.client()
+
+def add_score(name, score):
+    db = get_firestore()
+    scoreboard = db.collection('scoreboard').add({
+        'name': name,
+        'score': score,
+        'timestamp': firestore.SERVER_TIMESTAMP
+    })
+    return scoreboard
+
+def get_scores():
+    db = get_firestore()
+    scores = db.collection('scoreboard').order_by('score', direction=firestore.Query.DESCENDING).limit(10).stream()
+    return scores
+
+def add_and_get_scores(name, score):
+    add_score(name, score)
+    return get_scores()
--- functions/main.py
+++ functions/main.py
@@ -7,6 +7,7 @@ from firebase_admin import initialize_app
 from bingocard import ez_bingo_card
 from flask import jsonify
 from llm import get_gpt4_response
+from db import add_and_get_scores
 
 initialize_app()
 
@@ -42,8 +43,8 @@ def prompt(req: https_fn.Request) -> https_fn.Response:
     if req.method == 'OPTIONS':
         headers = {
             'Access-Control-Allow-Origin': 'http://localhost:3000',
-            'Access-Control-Allow-Methods': 'POST, OPTIONS', 
-            'Access-Control-Allow-Headers': 'Content-Type', 
+            'Access-Control-Allow-Methods': 'POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Max-Age': '3600',
         }
         return ('', 204, headers)
@@ -55,3 +56,28 @@ def prompt(req: https_fn.Request) -> https_fn.Response:
     prompt = req_json['prompt']
     response = get_gpt4_response(prompt)
     return jsonify({ 'text': response })
+
+@https_fn.on_request(
+    cors=options.CorsOptions(
+        cors_origins=["http://localhost:3000"],
+        cors_methods=["post", "options"],
+    )
+)
+def score(req: https_fn.Request) -> https_fn.Response:
+    if req.method == 'OPTIONS':
+        headers = {
+            'Access-Control-Allow-Origin': 'http://localhost:3000',
+            'Access-Control-Allow-Methods': 'POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600',
+        }
+        return ('', 204, headers)
+    headers = {
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
+    }
+
+    req_json = req.get_json()
+    name = req_json['name']
+    score = req_json['score']
+    scores = add_and_get_scores(name, score)
+    return jsonify({ 'scores': scores })
